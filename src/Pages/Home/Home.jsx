6c49ca6af56b15facf8586d0a05cf11f54bdb98d
Pamel-C223171 <Mohammadpamel12345@gmail.com>
2025-11-18 00:07:19 +0300

    update to the home page and added to the our platform section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -119,8 +119,42 @@ const Home = () => {
 
             </div>
 
-          
 
+
+            <div className='py-14 bg-base-200'>
+
+                <div className=' flex flex-col items-center '>
+
+                    <h1 className="text-5xl text-center  font-bold">About Our Flatform</h1>
+                    <p className="text-center mt-5 "> Our platform is designed to make learning easy, smooth, and accessible for everyone. <br />
+                        Whether you are a beginner or an advanced learner, we provide structured resources,
+                        clean UI, and a seamless experience.
+                    </p>
+                    <div className="join mt-8">
+                        <div>
+                            <label className="input validator join-item">
+                                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <g
+                                        strokeLinejoin="round"
+                                        strokeLinecap="round"
+                                        strokeWidth="2.5"
+                                        fill="none"
+                                        stroke="currentColor"
+                                    >
+                                        <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                    </g>
+                                </svg>
+                                <input type="email" placeholder="Enter your email" required />
+                            </label>
+                            <div className="validator-hint hidden">Enter valid email address</div>
+                        </div>
+                        <button className="btn btn-primary hover:bg-black join-item">Join</button>
+                    </div>
+                </div>
+
+
+            </div>
 
 
 
